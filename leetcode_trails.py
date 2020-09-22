27406879f6425166821c62f3484afee03f057154
rajesh333-3 <venkatadurgarajesh.thoomu@gmail.com>
2020-09-22 19:36:46 +0530

    fist commit in my life

---
+++ leetcode_trails.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 26 17:42:05 2020
+
+@author: VenkataDurgaRajesh
+"""
+
+l=list("1,2,3,4,5,6".split(','))
+print(l[0])
+
+def twoSum(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if(nums[i]+nums[j] == target and i != j):
+                return [i,j]
+                break
+ans=twoSum([3,3],6)
+print(ans)
